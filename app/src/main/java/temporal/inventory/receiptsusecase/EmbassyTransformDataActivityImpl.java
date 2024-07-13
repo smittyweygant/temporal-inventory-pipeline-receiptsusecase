d2dcc42b78ecb29f570f5ccb8f1e494c73c8fd33
GeethaAnne <geethaanne.sjsu@gmail.com>
2024-07-13 15:23:05 -0500

    Update EmbassyTransformDataActivityImpl.java

--- app/src/main/java/temporal/inventory/receiptsusecase/EmbassyTransformDataActivityImpl.java
+++ app/src/main/java/temporal/inventory/receiptsusecase/EmbassyTransformDataActivityImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DataProcessingActivitiesImpl implements DataProcessingActivities {
+public class EmbassyTransformDataActivityImpl implements EmbassyTransformDataActivity {
     @Override
     public void processRecord(String eventType, String record) {
         // Implement your processing logic here
