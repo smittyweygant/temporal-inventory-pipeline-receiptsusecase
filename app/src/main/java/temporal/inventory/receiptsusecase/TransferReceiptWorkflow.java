c43357b2ffbd4d918e0bdf2ef81eb3f181131c51
GeethaAnne <geethaanne.sjsu@gmail.com>
2024-07-13 15:22:29 -0500

    Update TransferReceiptWorkflow.java

--- app/src/main/java/temporal/inventory/receiptsusecase/TransferReceiptWorkflow.java
+++ app/src/main/java/temporal/inventory/receiptsusecase/TransferReceiptWorkflow.java
@@ -1,5 +1,11 @@
 package temporal.inventory.receiptsusecase;
 
-public class TransferReceiptWorkflow {
-    
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+import java.util.List;
+
+@WorkflowInterface
+public interface TransferReceiptWorkflow {
+  @WorkflowMethod
+  void processBatch(List<String> batch);
 }
